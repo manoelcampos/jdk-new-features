8ee82e714304062837f8340b914cc8759f5856ac
Manoel Campos <manoelcampos@gmail.com>
2022-10-22 21:57:47 -0300

    Introduces JDK 9 Collections Factories sample
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/samples/jdk09/Jdk09.java
+++ src/main/java/samples/jdk09/Jdk09.java
@@ -1,7 +1,29 @@
 package samples.jdk09;
 
-public class Jdk09 {
-    public static void main(String[] args) {
+import samples.jdk16.Country;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Jdk09 {
+    /**
+     * Shows how to use Collection Factories
+     * @param args
+     */
+    public static void main(String[] args) {
+        List<Integer> numberList = List.of(1, 2, 3, 4);
+        System.out.println(numberList);
+
+        List<Country> countryList = List.of(
+            new Country("Brazil", "South America"),
+            new Country("Portugal", "Europe")
+        );
+        System.out.println(countryList);
+
+        Map<Integer, String> numbersMap = Map.of(1, "One", 2, "Two", 3, "Three");
+
+        Set<Character> charSet = Set.of('a', 'e', 'i', 'o', 'u');
+        System.out.println(charSet);
     }
 }
