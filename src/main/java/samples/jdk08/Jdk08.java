5babef3c0c2b0d2394d7e80d42b62f7847a94273
Manoel Campos <manoelcampos@gmail.com>
2023-03-16 17:37:04 -0300

    Introduces MultipleInheritance example with default methods on interfaces.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/samples/jdk08/Jdk08.java
+++
@@ -1,7 +0,0 @@
-package samples.jdk08;
-
-public class Jdk08 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/samples/jdk08/MultipleInheritance.java
@@ -0,0 +1,99 @@
+package samples.jdk08;
+
+import java.util.regex.Pattern;
+
+/**
+ * Changes the chars into a String to anoter representation.
+ */
+interface StringEncoder {
+    String convert(String text);
+}
+
+/**
+ * Replaces some letters by numbers.
+ * <a href="https://en.wikipedia.org/wiki/Leet">Leet</a>
+ */
+interface LeetCode extends StringEncoder {
+    // Partial list of leet codes.
+    String[][] leetCodes = {
+            {"A", "4"},
+            {"B", "8"},
+            {"C", "(", "{"},
+            {"D", "|)"},
+            {"E", "3"},
+            {"F", "ph"},
+            {"G", "6"},
+            {"H", "#"},
+            {"I", "1"},
+            {"J", "j"},
+            {"K", "|<"},
+            {"O", "0"}};
+
+    @Override
+    default String convert(String text) {
+        for (final String[] row : leetCodes) {
+            text = text.replaceAll(row[0], row[1]);
+        }
+
+        return text;
+    }
+}
+
+/**
+ * Capitalizes every first letter into a word.
+ */
+class Camelcaser implements StringEncoder {
+    @Override
+    public String convert(String text) {
+        final var matcher = Pattern.compile("(^[a-z])|\\s([a-z])").matcher(text);
+        return matcher.replaceAll(r -> r.group(0).toUpperCase());
+    }
+}
+
+/**
+ * Converts the String to uppercase
+ */
+interface Uppercaser extends StringEncoder {
+    @Override
+    default String convert(String text) {
+        return text.toUpperCase();
+    }
+}
+
+/**
+ * Converts String into uppercase then to Leet code
+ * using the classes {@link Uppercaser} and {@link LeetCode}.
+ * Since those classes have a default implementation for the convert method,
+ * this UpperLeet class could not use both of them.
+ * If you just remove the convert method below
+ * you'll see the method doesn't compile.
+ *
+ * <p>
+ * Overriding the method, you can decide which implementation to use.
+ * In this example, I decided to use both.
+ * </p>
+ */
+class UpperLeet implements Uppercaser, LeetCode {
+    @Override
+    public String convert(String text) {
+        return LeetCode.super.convert(Uppercaser.super.convert(text));
+    }
+}
+
+/**
+ * Shows how to use multiple inheritance in JDK 8 with
+ * default methods in interfaces.
+ * Thoses methods have a default implementation
+ * that are inherited by implementing classes.
+ * The {@link UpperLeet} class applies multiple
+ * inheritance by implementing the {@link Uppercaser}
+ * and {@link LeetCode} interfaces.
+ */
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        final var str = "a string with all letters lower case, just to try";
+        System.out.printf("Original   String: %s%n", str);
+        System.out.printf("Camelcase  String: %s%n", new Camelcaser().convert(str));
+        System.out.printf("Upper Leet String: %s%n", new UpperLeet().convert(str));
+    }
+}
